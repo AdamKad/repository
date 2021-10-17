2c8d578eb14fb3290f413afb17cc65cf35c7dfef
AKadluczka <kadluczka.adam@gmail.com>
2021-10-17 11:28:17 +0200

    fix pojazd

--- Nowy folder/Kalkulator/Kalkulator/Program.cs
+++ Nowy folder/Kalkulator/Kalkulator/Program.cs
@@ -18,8 +18,15 @@ namespace Kalkulator
 
             Console.WriteLine("Podaj aktualną pogodę: 1-słońce, 2-deszcz, 3-śnieg");
             Int32.TryParse(Console.ReadLine(), out pogoda);
-            Console.WriteLine("Podaj wybrany pojazd: 1-pieszo, 2-rower, 3-auto");
-            Int32.TryParse(Console.ReadLine(), out pojazd);
+            do
+            {
+                Console.WriteLine("Podaj wybrany pojazd: 1-pieszo, 2-rower, 3-auto");
+                Int32.TryParse(Console.ReadLine(), out pojazd);
+            }
+            while(pojazd < 1 || pojazd > 3);
+
+
+            if (pojazd < 1 || pojazd > 3) Console.WriteLine("pojazd spoza zakresu");
             Console.WriteLine("Podaj dystans (0-10km)");
             Double.TryParse(Console.ReadLine(), out dystans);
 
@@ -58,9 +65,9 @@ namespace Kalkulator
             {
                 Console.WriteLine("Podałeś błędnie pogodę");
             }
-            
+
             double mnoznik = 1;
-          
+
             if (pogoda == 2) mnoznik = 1.5;
             else if (pogoda == 3) mnoznik = 2.2;
 
